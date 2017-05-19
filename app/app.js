36c1c3dbc9c7a0100ed60ee3ea927119d8909f25
agent47 <1988oliver>
2017-05-19 13:14:55 +0200

    Init Handlebars

---
+++ app/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by oliver on 07.05.17.
+ */
+
+var express = require('express');
+var exbars  = require('exbars');
+var app = express();
+
+//statische Dateien einbinden
+app.use('/static', express.static(__dirname + '/public'));
+
+//Template Engine registrieren
+app.engine('hbs', exbars({
+    defaultLayout: 'main',
+    viewsPath:     'app/views'
+}));
+app.set('views', 'app/views');
+app.set('view engine', 'hbs');
+
+//Routen
+require('./routes/movie')(app);
+
+//Server starten
+app.listen(3000, function () {
+    console.log('Server unter http://localhost:3000 gestartet');
+});
